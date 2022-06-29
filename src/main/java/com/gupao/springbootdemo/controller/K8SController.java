53e099d40c34c859da04be99a031b680a3f2ce17
zhai.wenlong <1771123018@qq.com>
2022-06-29 10:21:48 +0800

    去玩儿

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -11,6 +11,6 @@ public class K8SController {
 
     @RequestMapping("/k8s")
     public String k8s(){
-        return "hello K8s <br/>11122222  233 ";
+        return "hello K8s <br/>111  22222  233 ";
     }
 }
